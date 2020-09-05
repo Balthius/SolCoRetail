cc6c504154dd41319deb34061d87f0a86846dfd8
Josh Folsom <josh.folsom@yahoo.com>
2020-09-04 17:56:55 -0700

    Add WPF Project with MVVM
    Adds WPF UI and fonfigures with Caliburn
    Micro for our MVVM framework
    also includes simple container DI system

---
+++ SRMDesktopUI/Bootstrapper.cs
@@ -0,0 +1,57 @@
+﻿using Caliburn.Micro;
+using SRMDesktopUI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SRMDesktopUI
+{
+    class Bootstrapper :BootstrapperBase
+    {
+
+        private SimpleContainer _container = new SimpleContainer();
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+        protected override void Configure()
+        {
+            _container.Instance(_container);
+
+            _container
+                .Singleton<IWindowManager, WindowManager>()
+                .Singleton<IEventAggregator, EventAggregator>();
+            //in our assembly get every type, that is a class, that ends in viewmodel, put it in a list, and then for each one
+            //register each one to a container
+            //auto-wires up all viewmodels. makes unit testing a little harder. good way to start though (MVP)
+            GetType().Assembly.GetTypes()
+                .Where(type => type.IsClass)
+                .Where(type => type.Name.EndsWith("ViewModel"))
+                .ToList()
+                .ForEach(viewModelType => _container.RegisterPerRequest(
+                    viewModelType, viewModelType.ToString(), viewModelType));
+        }
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return _container.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
+        }
+    }
+}
